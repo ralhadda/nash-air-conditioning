d8c13f7b70f167050acec9c7035e2243bf841212
Ramiz Alhaddad <ramizalhaddad@Ramizs-MacBook-Air.local>
2024-07-28 14:59:04 -0700

    nash heading first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   const images = document.querySelectorAll('.rotating-image');
+//   let currentIndex = 0;
+
+//   function showNextImage() {
+//     images[currentIndex].style.display = 'none';
+//     currentIndex = (currentIndex + 1) % images.length;
+//     images[currentIndex].style.display = 'block';
+//   }
+
+//   setInterval(showNextImage, 3000); // Change image every 3 seconds
+
+//   // Show the first image initially
+//   images[currentIndex].style.display = 'block';
+// });
